5aa45401c075c7cccff9ac93f478c067b2fbda39
Kravchuk_ris <i.kravchuk@risgroup.ru>
2024-06-27 14:22:17 +0200

    map

---
+++ client/templates/default/components/Map/styled.ts
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const TitleBlock = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-bottom: 40px;
+  border-bottom: 1px solid #4f4f4f;
+  width: 100%;
+  max-width: 1440px;
+`;
+
+export const Text = styled.div`
+  font-size: 32px;
+  font-weight: 700;
+  line-height: 43.71px;
+  letter-spacing: 0.02em;
+`;
+
+export const BlockMap = styled.div`
+  padding-top: 47px;
+`;
